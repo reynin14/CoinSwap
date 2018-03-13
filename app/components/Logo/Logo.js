9a6352109cc7b6b31a4429e29e6421c9b4821098
reynin14 <kyonzon14@gmail.com>
2018-03-12 22:57:19 -0700

    logo created and rendering

---
+++ app/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+
+const Logo = () => (
+  <View>
+    <Image source={require('./images/CoinSwapLogo.png')}/>
+  </View>
+);
+
+export default Logo;
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -3,9 +3,11 @@ import { View, StatusBar } from 'react-native';
 
 import { Container } from '../components/Container';
 
+import { Logo } from '../components/Logo';
+
 export default () => (
   <Container>
     <StatusBar translucent={false} barStyle="light-content"/>
-    <View />
+    <Logo />
   </Container>
 );
