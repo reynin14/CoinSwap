719f00a0559aed62a4e2214900498633089dacd2
reynin14 <kyonzon14@gmail.com>
2018-03-15 11:59:00 -0700

    last conversion rate and date added

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -5,12 +5,15 @@ import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput'
 import { ClearButton } from '../components/Buttons'
+import { LastConverted } from '../components/Text'
 
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
 const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '79.74';
+const TEMP_CONVERSION_RATE = 0.7974;
+const TEMP_CONVERSION_DATE = new Date();
 
 class Home extends React.Component {
   handlePressBaseCurrency = () => {
@@ -47,10 +50,16 @@ class Home extends React.Component {
 					editable={false}
 					value={TEMP_QUOTE_PRICE}
 				/>
-      <ClearButton
-        text="Reverse Currencies"
-        onPress={this.handleSwapCurrency}
-      />
+        <LastConverted
+          base={TEMP_BASE_CURRENCY}
+          quote={TEMP_QUOTE_CURRENCY}
+          date={TEMP_CONVERSION_DATE}
+          conversionRate={TEMP_CONVERSION_RATE}
+        />
+        <ClearButton
+          text="Reverse Currencies"
+          onPress={this.handleSwapCurrency}
+        />
 
       </Container>
     );
